67a51cc23f2a00f559a04996a188787df410c5d1
Anirudh <140832676+aniupa@users.noreply.github.com>
2026-02-14 02:11:37 +0530

    feat: updated ui for desktop users

--- frontend/src/components/header/Header.jsx
+++ frontend/src/components/header/Header.jsx
@@ -3,35 +3,51 @@ import { useFilterTasks } from "../../hooks/filterTasks";
 import { useContext, useState } from "react";
 import { MyContext } from "../../context/context";
 import { useEffect } from "react";
+import { MoonIcon } from "../../utilits/icons/icons";
 const Header = () => {
   const { inputVal, setInputVal } = useContext(MyContext);
-  const [ theme, setTheme ] = useState('dark');
+  const [theme, setTheme] = useState("dark");
   const { addNewTask } = useFilterTasks(inputVal, setInputVal);
- const toggleTheme=()=>{
-  setTheme(t=>t==='dark'? 'light':'dark');
- }
- useEffect(()=>{
- document.documentElement.dataset.theme=theme;
-},[theme]);
+  const toggleTheme = () => {
+    setTheme((t) => (t === "dark" ? "light" : "dark"));
+  };
+  useEffect(() => {
+    document.documentElement.dataset.theme = theme;
+  }, [theme]);
   return (
     <header>
-      <span
-        style={{
-          display: "flex",
-          width: "100%",
-          justifyContent: "space-between",
-        }}
-      >
-        <span>Todo list</span> <span onClick={toggleTheme}>toggle</span>
+      <span className="heroToggleContainer">
+        <span style={{ fontSize: "var(--font-lg)" }}>Todo list</span>{" "}
+        <span style={{ width: "20px" }} onClick={toggleTheme}>
+          {MoonIcon}
+        </span>
       </span>
-      <section>
-        <input className="taskInput"
+      <section className="addTasksMb">
+        <input
+          className="taskInput"
           type="text"
           id="add"
           placeholder="add new task"
           value={inputVal}
           onChange={(e) => setInputVal(e.target.value)}
           onKeyDown={(e) => e.key == "Enter" && addNewTask()}
+        />{" "}
+        {/* <br /> */}
+        <button type="submit" onClick={addNewTask}>
+          Add
+        </button>
+      </section>
+      <section className="addTasksPc">
+        <label htmlFor="add">Add new task</label>
+        <input
+          className="taskInput"
+          type="text"
+          id="add"
+          placeholder=" eg: buy milk "
+          value={inputVal}
+          onChange={(e) => setInputVal(e.target.value)}
+          onKeyDown={(e) => e.key == "Enter" && addNewTask()}
+          
         />{" "}
         {/* <br /> */}
         <button type="submit" onClick={addNewTask}>
--- frontend/src/pages/home/Home.jsx
+++ frontend/src/pages/home/Home.jsx
@@ -3,9 +3,10 @@ import Header from "../../components/header/Header.jsx";
 import "./home.css";
 import { useFilterTasks } from "../.././hooks/filterTasks.jsx";
 import { MyContext } from "../../context/context.jsx";
-
+import { LeftArrowIcon, RightArrowIcon } from "../../utilits/icons/icons.jsx";
 const Home = () => {
   const [filterPg, setFilterPg] = useState(1);
+  const [filterPgPc, setFilterPgPc] = useState("all");
 
   const { tasks, setTasks, inputVal, setInputVal } = useContext(MyContext);
   const { pendingList, completeList, renderList } = useFilterTasks(
@@ -16,66 +17,83 @@ const Home = () => {
   );
 
   return (
-    <div className="container">
-      <Header />
-      <div
-        style={{
-          display: "flex",
-          flexDirection: "row",
-          justifyContent: "space-evenly",
-        }}
-      >
-        <section>
-          {" "}
-          <span
-            onClick={() =>
-              filterPg >= 2 ? setFilterPg(filterPg - 1) : filterPg
-            }
-          >
-            <svg
+    <div className="mainContainer">
+      <div className="container">
+        <Header />
+        {/* mobile styled filter */}
+        <div className="filterToggleMb">
+          <section>
+            {" "}
+            <span
+              onClick={() =>
+                filterPg >= 2 ? setFilterPg(filterPg - 1) : filterPg
+              }
               style={{
-                width: "15px",
-                color: `${filterPg > 1 ? "red" : "grey"}`,
+                color: `${filterPg > 1 ? "red" : "var(--text-dim)"}`,
               }}
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 24 24"
-              fill="currentColor"
             >
-              <path d="M10.8284 12.0007L15.7782 16.9504L14.364 18.3646L8 12.0007L14.364 5.63672L15.7782 7.05093L10.8284 12.0007Z"></path>
-            </svg>
-          </span>
-          <span style={{ margin: " var(--margin-sm)" }}>
-            {filterPg === 1 && "all tasks"}
-            {filterPg === 2 && "pending tasks"}
-            {filterPg === 3 && "completed tasks"}
-          </span>
-          <span
-            onClick={() =>
-              filterPg <= 2 ? setFilterPg(filterPg + 1) : filterPg
-            }
-          >
-            <svg
+              {LeftArrowIcon}
+            </span>
+            <span style={{ margin: " var(--margin-sm)" }}>
+              {filterPg === 1 && "all tasks"}
+              {filterPg === 2 && "pending tasks"}
+              {filterPg === 3 && "completed tasks"}
+            </span>
+            <span
+              onClick={() =>
+                filterPg <= 2 ? setFilterPg(filterPg + 1) : filterPg
+              }
               style={{
-                width: "15px",
-                color: `${filterPg < 3 ? "red" : "grey"}`,
+                color: `${filterPg < 3 ? "red" : "var(--text-dim)"}`,
               }}
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 24 24"
-              fill="currentColor"
             >
-              <path d="M13.1717 12.0007L8.22192 7.05093L9.63614 5.63672L16.0001 12.0007L9.63614 18.3646L8.22192 16.9504L13.1717 12.0007Z"></path>
-            </svg>
-          </span>{" "}
+              {RightArrowIcon}
+            </span>{" "}
+          </section>
+        </div>
+        {/* Pc styled filter */}
+        <section className="tasksContainerPc">
+          <div className="left">
+            <h3>Stats & filters</h3>
+            <hr />
+            <br />
+            <span
+              onClick={() => setFilterPgPc("pending")}
+              className={filterPgPc === "pending" ? "isActive" : {}}
+            >
+              Pending :
+            </span>
+            <br />
+            <span
+              className={filterPgPc === "all" ? "isActive" : {}}
+              onClick={() => setFilterPgPc("all")}
+            >
+              All tasks :
+            </span>
+            <br />
+            <span
+              className={filterPgPc === "completed" ? "isActive" : {}}
+              onClick={() => setFilterPgPc("completed")}
+            >
+              Completed tasks :
+            </span>
+          </div>
+          <div className="right">
+            {/* pc filter tasks container */} <h2>{filterPgPc} Tasks</h2><hr />
+            
+            {(filterPgPc === "pending" && pendingList) ||
+              (filterPgPc === "all" && renderList) ||
+              (filterPgPc === "completed" && completeList)}
+          </div>
+          {/*  */}
         </section>
-      </div>
-      <section className="tasksContainer">
-        {" "}
-        {
-          (filterPg === 1 && renderList )||
+        <section className="tasksContainerMb">
+          {" "}
+          {(filterPg === 1 && renderList) ||
             (filterPg === 2 && pendingList) ||
             (filterPg === 3 && completeList)}
-      
-      </section>
+        </section>
+      </div>
     </div>
   );
 };
